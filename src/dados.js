d8989287e3d283f3a335d296057dcb627db7f306
paulo <paulo.s.2011@hotmail.com.br>
2020-03-10 00:49:38 -0300

    Commit Final

---
+++ src/dados.js
@@ -0,0 +1,14 @@
+export function loadLists() {
+  return [
+    {
+      rbase: 1,
+      numero: '400',
+      tipo_logradouro: 'Avenida',
+      logradouro: 'Av. Sete de Setembro',
+      complemento: '',
+      bairro: 'Dois de Julho,',
+      cidade: 'Salvador,',
+      estado: 'Bahia',
+    },
+  ];
+}
